ed539148f770458db4c2a67e63e7c29cc625b74d
Eduardo Zago <zagoe22@gmail.com>
2024-12-09 21:05:54 -0300

    refactor: add clean architecture on folders structure

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,11 +1,9 @@
 import { AnswerQuestionUseCase } from './answer-question'
-import { AnswersRepository } from '../repositories/answers-repository'
-import { Answer } from '../entities/answer'
+import { AnswersRepository } from '@/repositories/answers-repository'
+import { Answer } from '@/enterprise/entities/answer'
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return
-  },
+  create: async (answer: Answer) => {},
 }
 
 it('should be able to create an answer', async () => {
