632e09e75caa0cda7ae6dfa2ef2f34935a948f82
Eduardo Zago <zagoe22@gmail.com>
2024-12-12 19:10:06 -0300

    feat: implement edit question use case and tests

--- src/domain/forum/application/use-cases/delete-answer.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
-import { makeAnswer } from 'test/factories/make-answer'
+import { makeAnswer } from '@/../test/factories/make-answer'
 import { DeleteAnswerUseCase } from './delete-answer'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
